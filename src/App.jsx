c2aa3b01d5653d726841c03d9957baaff6510569
Arnab Golder <arnabgolder7@gmail.com>
2023-02-15 20:59:04 +0530

    uerefHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import { useState,useEffect } from 'react';
-import Parent from './components/Parent';
-import ReducerHook from './components/useReducer/ReducerHook';
-// import ClearUp from './components/ClearnUp';
+import React from 'react'
+import RefHook from './components/useRef/RefHook';
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // useEffect(()=>{
-  //   alert(count);
-  // },[]);
-  // const userData ={
-  //   name:'john',
-  //   age:16,
-  // };
+ 
   return (
-  <>
-  {/* <button onClick={()=>{setCount(count+1)}} >Click Me! {count}</button> */}
-  {/* <ClearUp/> */}
-   {/* <Parent /> */}
-   <ReducerHook />
-
-  </>
+       <>
+        <RefHook/>
+      </>
   )
 }
 
